e2e05dc8ab8e32b0bb9e63fd5a27f0f0e534a8ba
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-04-12 14:12:14 -0400

    Better login screen

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,11 +2,18 @@ import React, { useState, useEffect } from "react";
 import {connect} from 'react-redux';
 
 import { loginRequest } from "../actions/AuthActions";
+import { Button } from "@material-ui/core";
 
-const Login = ({ loginRequestD }) => (
-        <li style = {{display:"inline"}}>
-            <button onClick={() => loginRequestD()}>Meme</button>
-        </li>
+const Login = ({ loginRequest }) => (
+    <div style={{ height: '100vh', width: '100vw', display: 'flex', position: 'relative', textAlign: 'center', alignItems: 'center', justifyContent: 'center' }}>
+        <div style={{ display: "inline-block" }}>
+            <h3>the app formerly known as schedule planner</h3>
+            <h5>brought to you by riceapps</h5>
+        </div>
+        <div style={{ position: 'absolute', marginTop: '75px' }}>
+            <Button variant="outlined" onClick={() => loginRequest()}>Enter</Button>
+        </div>
+    </div>
 )
 
 export default connect(
@@ -15,7 +22,7 @@ export default connect(
     }),
     (dispatch) => {
         return ({
-            loginRequestD: () => dispatch(loginRequest()),
+            loginRequest: () => dispatch(loginRequest()),
         });
     },
 )(Login);
