d4217a30473086b01d2633ae7bcd59d84e26d623
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-04-13 16:57:57 -0400

    Better loading screen for after login; QoL change

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -2,14 +2,25 @@ import React, { useState, useEffect } from "react";
 import {connect} from 'react-redux';
 
 import { authenticateRequest } from "../actions/AuthActions";
+import { Backdrop, CircularProgress, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: '#fff',
+    },
+  }));
 
 const Auth = ({ authenticateRequest }) => {
+    const classes = useStyles();
     console.log("Inside auth component");
     
     authenticateRequest();
 
     return (
-        <p>Wait...</p>
+        <Backdrop className={classes.backdrop} open={true}>
+            <CircularProgress color="inherit" />
+        </Backdrop>
     )
 }
 
