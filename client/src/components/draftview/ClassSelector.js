e82917b146f821d629925ce3471e5a5ed09764d9
jlt10 <jamietan221@gmail.com>
2020-04-16 13:10:55 -0500

    Created DraftCourseItem component from ClassSelector

--- client/src/components/draftview/ClassSelector.js
+++ client/src/components/draftview/ClassSelector.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React from "react";
 import {connect} from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -8,24 +8,12 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-// Course evals
-import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
-// Course visible
-import Checkbox from '@material-ui/core/Checkbox';
-// Has lab
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-// Delete course
-import DeleteIcon from '@material-ui/icons/Delete';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
 import SwipeableViews from "react-swipeable-views";
 
 // Tracking
-import ReactGA from "react-ga";
 import {toggleCourseRequest, removeCourseRequest} from '../../actions/CoursesActions';
 import { initGA, Event } from "../../utils/analytics";
-import { classTimeString } from '../../utils/CourseTimeTransforms';
-import URLTypes from "../../constants/URLTypes";
+import DraftCourseItem from "./DraftCourseItem";
 
 
 const useStyles = makeStyles({
@@ -34,39 +22,18 @@ const useStyles = makeStyles({
 	}
   });
 
-const createURL = (termcode, crn, type=URLTypes.DETAIL) => {
-	switch (type) {
-		case URLTypes.DETAIL:
-			return `https://courses.rice.edu/courses/!SWKSCAT.cat?p_action=COURSE&p_term=${termcode}&p_crn=${crn}`;
-		case URLTypes.EVAL:
-			return `https://esther.rice.edu/selfserve/swkscmt.main?p_term=${termcode}&p_crn=${crn}&p_commentid=&p_confirm=1&p_type=Course`;
-		default:
-			console.log(`Uknown URL type: ${type}`);
-			return "https://rice.edu/"
-	}
-}
+  const styles = {
+	  slideContainer: {
+	    maxHeight: '50vh',
+	    maxWidth: '100vw',
+	    WebkitOverflowScrolling: 'touch', // iOS momentum scrolling
+	  },
+  };
 
 const ClassSelector = ({draftCourses, toggleCourseRequest, removeCourseRequest}) => {
 	const classes = useStyles();
-
 	// Get headers
 	let headers = ["Visible", "Course Code", "Class Days", "Class Time", "CRN", "Lab Days", "Lab Times", "Instructor(s)", "Remove"]
-
-	const styles = {
-		slideContainer: {
-		  maxHeight: '50vh',
-		  maxWidth: '100vw',
-		  WebkitOverflowScrolling: 'touch', // iOS momentum scrolling
-		},
-	  };
-
-	const emptyCellGenerator = (count) => {
-		let cells = [];
-		for (let i = 0; i < count; i++) {
-			cells.push(<TableCell align="right"></TableCell>);
-		}
-		return cells;
-	}
 
 	// Initialize GA before use
 	initGA();
@@ -75,7 +42,6 @@ const ClassSelector = ({draftCourses, toggleCourseRequest, removeCourseRequest})
 		let crnString = String.toString(course.crn);
 		// Tracking 
 		Event("COURSE_SELECTOR", "Remove Course from Schedule: " + crnString, crnString);
-
 		// Remove course
 		removeCourseRequest(course)
 	}
@@ -96,68 +62,17 @@ const ClassSelector = ({draftCourses, toggleCourseRequest, removeCourseRequest})
 					</TableRow>
 				</TableHead>
 				<TableBody>
-					{draftCourses.map((course) => (
-						<TableRow key={course.crn}>
-							<TableCell padding="checkbox">
-								<Checkbox
-								checked={course.visible}
-								onClick={() => toggleCourseRequest(course)}
-								/>
-							</TableCell>
-							<TableCell align="right" component="th" scope="row">
-								<Tooltip title="View Course Details">
-									<ReactGA.OutboundLink style={{ color: "#272D2D", textDecoration: 'none' }} eventLabel="course_description" to={createURL("202110", course.crn, URLTypes.DETAIL)} target="_blank">
-										<span style={{ color: "272D2D" }}>{course.courseName}</span>
-									</ReactGA.OutboundLink>
-									{/* <a href={createURL(course.crn)} target="_blank" style={{ color: '#272D2D' }}></a> */}
-								</Tooltip>
-								<Tooltip title="View Evaluations">
-									<ReactGA.OutboundLink eventLabel="course_evaluation" to={createURL("202110", course.crn, URLTypes.EVAL)} target="_blank">
-										<IconButton aria-label="evaluations">
-											<QuestionAnswerIcon />
-										</IconButton>
-									</ReactGA.OutboundLink>
-								</Tooltip>
-							</TableCell>
-							{course.class.hasClass ? (
-								<Fragment>
-									<TableCell align="right">{course.class.days}</TableCell>
-									<TableCell align="right">{classTimeString(course.class.startTime, course.class.endTime)}</TableCell>
-								</Fragment>
-							) : <Fragment>{emptyCellGenerator(2)}</Fragment>}
-							<TableCell align="right">{course.crn}</TableCell>
-							{course.lab.hasLab ? (
-								<Fragment>
-									<TableCell align="right">{course.lab.days}</TableCell>
-									<TableCell align="right">{classTimeString(course.lab.startTime, course.lab.endTime)}</TableCell>
-								</Fragment>
-							) : <Fragment>{emptyCellGenerator(2)}</Fragment>}
-							<TableCell align="right">{course.instructors.join(" | ")}</TableCell>
-							<TableCell align="right">
-								<Tooltip title="Delete">
-									<IconButton aria-label="delete" onClick={() => handleCourseRemoveRequest(course)}>
-										<DeleteIcon />
-									</IconButton>
-								</Tooltip>
-							</TableCell>
-						</TableRow>
-					))}
+				{draftCourses.map((course) => (
+					<DraftCourseItem 
+						course={course}
+						onToggle={toggleCourseRequest}
+						onRemove={handleCourseRemoveRequest}/>
+				))}
 				</TableBody>
 			</Table>
 		</SwipeableViews>
 		</TableContainer>
 	)
-	// return (
-	// 	<List>
-	// 		{courses.map(course => {
-	// 			return (
-	// 				<ListItem key={course.crn} onClick={() => toggleCourse(course.crn)}>
-	// 					{course.courseName}
-	// 				</ListItem>
-	// 			)
-	// 		})}
-	// 	</List>
-	// )
 }
 
 
---
+++ client/src/components/draftview/DraftCourseItem.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect, Fragment } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+// Course evals
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+// Course visible
+import Checkbox from '@material-ui/core/Checkbox';
+// Delete course
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+
+// Tracking
+import ReactGA from "react-ga";
+import { classTimeString } from '../../utils/CourseTimeTransforms';
+import URLTypes from "../../constants/URLTypes";
+
+const createURL = (termcode, crn, type=URLTypes.DETAIL) => {
+	switch (type) {
+		case URLTypes.DETAIL:
+			return `https://courses.rice.edu/courses/!SWKSCAT.cat?p_action=COURSE&p_term=${termcode}&p_crn=${crn}`;
+		case URLTypes.EVAL:
+			return `https://esther.rice.edu/selfserve/swkscmt.main?p_term=${termcode}&p_crn=${crn}&p_commentid=&p_confirm=1&p_type=Course`;
+		default:
+			console.log(`Uknown URL type: ${type}`);
+			return "https://rice.edu/"
+	}
+}
+
+const DraftCourseItem = ({ course, onToggle, onRemove }) => {
+	const emptyCellGenerator = (count) => {
+		let cells = [];
+		for (let i = 0; i < count; i++) {
+			cells.push(<TableCell align="right"></TableCell>);
+		}
+		return cells;
+        }
+        const createCourseTimeCells = (courseTime, exists) => {
+                return exists ? (
+                        <Fragment>
+                                <TableCell align="right">{courseTime.days}</TableCell>
+                                <TableCell align="right">
+                                        {classTimeString(courseTime.startTime, courseTime.endTime)}
+                                </TableCell>
+                        </Fragment>) 
+                        : <Fragment>{emptyCellGenerator(2)}</Fragment>;
+        }
+
+        return (
+        <TableRow key={course.crn}>
+                <TableCell padding="checkbox">
+                        <Checkbox
+                        checked={course.visible}
+                        onClick={() => onToggle(course)}
+                        />
+                </TableCell>
+                <TableCell align="right" component="th" scope="row">
+                        <Tooltip title="View Course Details">
+                                <ReactGA.OutboundLink 
+                                style={{ color: "#272D2D", textDecoration: 'none' }} 
+                                eventLabel="course_description" 
+                                to={createURL("202110", course.crn, URLTypes.DETAIL)} 
+                                target="_blank">
+                                        <span style={{ color: "272D2D" }}>{course.courseName}</span>
+                                </ReactGA.OutboundLink>
+                        </Tooltip>
+                        <Tooltip title="View Evaluations">
+                                <ReactGA.OutboundLink 
+                                eventLabel="course_evaluation" 
+                                to={createURL("202110", course.crn, URLTypes.EVAL)} 
+                                target="_blank">
+                                        <IconButton aria-label="evaluations">
+                                        <QuestionAnswerIcon />
+                                        </IconButton>
+                                </ReactGA.OutboundLink>
+                        </Tooltip>
+                </TableCell>
+                {createCourseTimeCells(course.class, course.class.hasClass)}
+                <TableCell align="right">{course.crn}</TableCell>
+                {createCourseTimeCells(course.lab, course.class.hasLab)}
+                <TableCell align="right">{course.instructors.join(", ")}</TableCell>
+                <TableCell align="right">
+                        <Tooltip title="Delete">
+                                <IconButton aria-label="delete" onClick={() => onRemove(course)}>
+                                <DeleteIcon />
+                                </IconButton>
+                        </Tooltip>
+                </TableCell>
+        </TableRow>);
+}
+
+
+
+export default DraftCourseItem;
