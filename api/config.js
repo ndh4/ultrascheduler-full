d5ad90a88d8078d72086e915bc16bf64694201cf
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-04-18 01:43:34 -0400

    Environment variables working locally

--- api/config.js
+++ api/config.js
@@ -1,5 +1,10 @@
 // Import all env vars from .env file
 require('dotenv').config()
 
-export const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
-export const SERVICE_URL = process.env.SERVICE_URL;
+const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
+const SERVICE_URL = process.env.SERVICE_URL;
+
+module.exports = {
+    MONGODB_CONNECTION_STRING: MONGODB_CONNECTION_STRING,
+    SERVICE_URL: SERVICE_URL
+}
