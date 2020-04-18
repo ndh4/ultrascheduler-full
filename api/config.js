d455f0150532eb84dc94087106dacfcc2c7032e9
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-04-18 01:39:07 -0400

    Removed hardcode; using env variables now

---
+++ api/config.js
@@ -0,0 +1,5 @@
+// Import all env vars from .env file
+require('dotenv').config()
+
+export const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
+export const SERVICE_URL = process.env.SERVICE_URL;
