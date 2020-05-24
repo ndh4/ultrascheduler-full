6ef629d6147f66dc0a132f5ff31dac806ecb3dc4
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-05-24 01:41:34 -0400

    Backend should actually be at http://api-...

--- client/nwb.config.js
+++ client/nwb.config.js
@@ -3,9 +3,9 @@ module.exports = {
     webpack: {
         define: {
             'process.env.REACT_APP_SERVICE_URL': JSON.stringify("http://develop-dot-ultrascheduler.uc.r.appspot.com/auth"),
-            'process.env.REACT_APP_BACKEND_URL': JSON.stringify("http://develop-dot-ultrascheduler.uc.r.appspot.com/api"),
-            'process.env.REACT_APP_GRAPHQL_URL': JSON.stringify("http://develop-dot-ultrascheduler.uc.r.appspot.com/graphql"),
-            'process.env.REACT_APP_GRAPHQL_WS_URL': JSON.stringify("ws://develop-dot-ultrascheduler.uc.r.appspot.com/graphql")
+            'process.env.REACT_APP_BACKEND_URL': JSON.stringify("http://api-develop-dot-ultrascheduler.uc.r.appspot.com/api"),
+            'process.env.REACT_APP_GRAPHQL_URL': JSON.stringify("http://api-develop-dot-ultrascheduler.uc.r.appspot.com/graphql"),
+            'process.env.REACT_APP_GRAPHQL_WS_URL': JSON.stringify("ws://api-develop-dot-ultrascheduler.uc.r.appspot.com/graphql")
         },
         aliases: {
             'cldr$': 'cldrjs',
