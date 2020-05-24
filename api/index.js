35a0c3c5ad4ec9005b47934a54e82a47c6b71860
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-05-24 04:01:50 -0400

    I should've removed cors earlier; my comment said FOR DEV PURPOSES why didn't I listen to that...

--- api/index.js
+++ api/index.js
@@ -36,9 +36,9 @@ const server = new ApolloServer({
 var app = express();
 
 // Apply cors for dev purposes
-app.use(cors({
-  origin: "https://develop-flex-dot-ultrascheduler.uc.r.appspot.com"
-}))
+// app.use(cors({
+//   origin: "*"
+// }))
 
 // Add JWT so that it is AVAILABLE; does NOT protect all routes (nor do we want it to)
 // Inspiration from: https://www.apollographql.com/blog/setting-up-authentication-and-authorization-with-apollo-federation
