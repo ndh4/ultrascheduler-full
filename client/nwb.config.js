7491c756f5d972c97e4dd44c858c511597bdf9b3
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-05-24 01:29:53 -0400

    Testing http first since we'll need an SSL certificate for production to accomodate for wss

--- client/nwb.config.js
+++ client/nwb.config.js
@@ -2,9 +2,9 @@ module.exports = {
     type: 'react-app',
     webpack: {
         define: {
-            'process.env.REACT_APP_SERVICE_URL': JSON.stringify("https://develop-dot-ultrascheduler.uc.r.appspot.com/auth"),
-            'process.env.REACT_APP_BACKEND_URL': JSON.stringify("https://develop-dot-ultrascheduler.uc.r.appspot.com/api"),
-            'process.env.REACT_APP_GRAPHQL_URL': JSON.stringify("https://develop-dot-ultrascheduler.uc.r.appspot.com/graphql"),
+            'process.env.REACT_APP_SERVICE_URL': JSON.stringify("http://develop-dot-ultrascheduler.uc.r.appspot.com/auth"),
+            'process.env.REACT_APP_BACKEND_URL': JSON.stringify("http://develop-dot-ultrascheduler.uc.r.appspot.com/api"),
+            'process.env.REACT_APP_GRAPHQL_URL': JSON.stringify("http://develop-dot-ultrascheduler.uc.r.appspot.com/graphql"),
             'process.env.REACT_APP_GRAPHQL_WS_URL': JSON.stringify("ws://develop-dot-ultrascheduler.uc.r.appspot.com/graphql")
         },
         aliases: {
