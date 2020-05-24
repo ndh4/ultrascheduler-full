4c85a140453774541714de29a101290f5d335922
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-05-24 00:46:14 -0400

    Added environment variables to webpack

--- client/nwb.config.js
+++ client/nwb.config.js
@@ -1,6 +1,12 @@
 module.exports = {
     type: 'react-app',
     webpack: {
+        define: {
+            'process.env.REACT_APP_SERVICE_URL': JSON.stringify("https://develop-dot-ultrascheduler.uc.r.appspot.com/auth"),
+            'process.env.REACT_APP_BACKEND_URL': JSON.stringify("https://develop-dot-ultrascheduler.uc.r.appspot.com/api"),
+            'process.env.REACT_APP_GRAPHQL_URL': JSON.stringify("https://develop-dot-ultrascheduler.uc.r.appspot.com/graphql"),
+            'process.env.REACT_APP_GRAPHQL_WS_URL': JSON.stringify("ws://develop-dot-ultrascheduler.uc.r.appspot.com/graphql")
+        },
         aliases: {
             'cldr$': 'cldrjs',
             'cldr': 'cldrjs/dist/cldr',
