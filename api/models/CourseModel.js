a7f889f3641e56e00b6eb3b95c5bad1b4f2685eb
Yanyu Zhong <yz133@rice.edu>
2020-07-09 19:24:02 -0500

    Co-authored-by: azhangyy <amz4@rice.edu> Co-authored-by: Angela Zhang <amz4@rice.edu>

--- api/models/CourseModel.js
+++ api/models/CourseModel.js
@@ -1,30 +1,34 @@
-var mongoose = require('mongoose')
-    , Schema = mongoose.Schema
+var mongoose = require("mongoose"),
+  Schema = mongoose.Schema;
 
-require('../db')
+require("../db");
 
-import { composeWithMongoose } from 'graphql-compose-mongoose';
-import composeWithDataloader from 'graphql-compose-dataloader';
+import { composeWithMongoose } from "graphql-compose-mongoose";
+import composeWithDataloader from "graphql-compose-dataloader";
 
 var RestrictionSchema = new Schema({
-    type: String,
-    setting: { type: String, enum: ['I', 'E'] }, // Only inclusive or exclusive
-    params: [String]
-})
+  type: String,
+  setting: { type: String, enum: ["I", "E"] }, // Only inclusive or exclusive
+  params: [String],
+});
 
 var CourseSchema = new Schema({
-    subject: String,
-    courseNum: Number,
-    longTitle: String,
-    creditsMin: Number,
-    creditsMax: Number,
-    restrictions: [ { type: RestrictionSchema } ],
-    prereqs: String,
-    coreqs: [ String ],
-    mutualExclusions: [ String ],
-    distribution: String
+  subject: String,
+  courseNum: Number,
+  longTitle: String,
+  creditsMin: Number,
+  creditsMax: Number,
+  restrictions: [{ type: RestrictionSchema }],
+  prereqs: String,
+  coreqs: [String],
+  mutualExclusions: [String],
+  distribution: String,
 });
 
 export const Course = mongoose.model("courses", CourseSchema);
-export const CourseTC = composeWithDataloader(composeWithMongoose(Course), { cacheExpiration: 3000, removeProjection: true, debug: true });
+export const CourseTC = composeWithDataloader(composeWithMongoose(Course), {
+  cacheExpiration: 3000,
+  removeProjection: true,
+  debug: true,
+});
 // export const CourseTC = composeWithMongoose(Course);
--- client/src/components/draftview/CourseDetail.js
+++ client/src/components/draftview/CourseDetail.js
@@ -3,7 +3,25 @@ import { Table, TableBody, TableRow, TableCell } from "@material-ui/core";
 import Collapse from "@material-ui/core/Collapse";
 
 /* Component for collapsible displaying prereqs and coreqs of a course*/
-const collapse = ({ course, open }) => {
+const collapse = ({
+    course,
+    session,
+    instructorsToNames,
+    open,
+    classTimeString,
+}) => {
+    const Times = (section) => {
+        if (!section.startTime || !section.endTime) {
+            return "None";
+        } else {
+            return (
+                <div>
+                    {section.days}{" "}
+                    {classTimeString(section.startTime, section.endTime)}
+                </div>
+            );
+        }
+    };
     return (
         <TableRow>
             <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={12}>
@@ -11,16 +29,91 @@ const collapse = ({ course, open }) => {
                     <Table>
                         <TableBody>
                             <TableCell>
-                                Prerequisites:{" "}
-                                {course.prereqs === ""
-                                    ? "None"
-                                    : course.prereqs}
+                                <div>
+                                    <b>Class Time: </b> {Times(session.class)}
+                                </div>
+                                <div>
+                                    <b>Lab Time: </b>
+                                    {Times(session.lab)}
+                                </div>
+                                <div>
+                                    <b> Instructor: </b>{" "}
+                                    {instructorsToNames(
+                                        session.instructors
+                                    ).join(", ")}
+                                </div>
+                                <div>
+                                    <b>Course Type: </b>
+                                    {}
+                                </div>
+                                <div>
+                                    <b>Distribution Group: </b>{" "}
+                                    {course.distribution}
+                                </div>
+                                <div>
+                                    <b>CRN:</b> {session.crn}
+                                </div>
                             </TableCell>
                             <TableCell>
-                                Corequisites:{" "}
-                                {course.coreqs.length === 0
-                                    ? "None"
-                                    : course.coreqs.join(", ")}
+                                <div>
+                                    <b>Section Max Enrollment: </b>
+                                    {session.maxEnrollment}
+                                </div>
+                                <div>
+                                    <b>Section Enrolled:</b>{" "}
+                                    {session.enrollment}
+                                </div>
+                                <div>
+                                    <b>Total Cross-list Max Enrollment: </b>{" "}
+                                    {console.log(
+                                        "maxenrollment",
+                                        session.maxCrossEnrollment
+                                    )}
+                                    {session.maxCrossEnrollment}
+                                </div>
+                                <div>
+                                    <b>Total Cross-list Enrolled:</b>{" "}
+                                    {session.crossEnrollment}
+                                </div>
+                                <div>
+                                    <b>Enrollment Restrictions:</b>{" "}
+                                    {console.log(
+                                        "restrictions",
+                                        course.restrictions
+                                    )}
+                                    {course.restrictions}
+                                </div>
+                                <div>
+                                    <b>Prerequisites:</b>{" "}
+                                    {course.prereqs === ""
+                                        ? "None"
+                                        : course.prereqs}
+                                </div>
+                                <div>
+                                    <b>Corequisites: </b>
+                                    {course.coreqs.length === 0
+                                        ? "None"
+                                        : course.coreqs.join(", ")}
+                                </div>
+                            </TableCell>
+
+                            <TableCell>
+                                <div>
+                                    <b>Long Title: </b>
+                                    {course.longTitle}
+                                </div>
+
+                                <div>
+                                    <b>Department:</b> {}
+                                </div>
+                                <div>
+                                    <b>Session:</b>
+                                    {course.term}
+                                </div>
+                                <div>
+                                    <b>Grade Mode: </b>
+                                    {}
+                                </div>
                             </TableCell>
                         </TableBody>
                     </Table>
