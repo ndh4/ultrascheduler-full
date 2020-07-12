acaf9dfee1581cfcb3097dbbcb7089cd53d5cd14
Thushar Mahesh <thushar.mahesh@gmail.com>
2020-07-12 14:03:38 -0400

    added the logout button

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -9,6 +9,7 @@ import { initGA, OutboundLink } from "../../utils/analytics";
 function Header() {
     // Where we collect feedback
     let feedbackURL = "https://forms.gle/6uyRuTxKgP3n53vB6";
+    let logoutURL = "https://idp.rice.edu/idp/profile/cas/logout"
 
     // This initializes Google Analytics
     initGA();
@@ -16,6 +17,10 @@ function Header() {
     // Redirects people to our Medium page on a new page if they click our logo to learn more about us
     const handleLogoClick = () => {
         OutboundLink("Clicked Logo.", window.open("https://medium.com/riceapps", "_blank"));
+    }
+    const handleLogoutClick = () => {
+        localStorage.removeItem("token")
+        window.open(logoutURL, "_self")
     }
 
     return (
@@ -32,6 +37,12 @@ function Header() {
                 style={styles.feedback}
                 onClick={() => window.open(feedbackURL, "_blank")}>
                 Feedback?
+                </Button>
+                <Button
+                 variant="outlined"
+                 style={styles.logoutButton}
+                 onClick={() => handleLogoutClick()}>
+                 Log Out
                 </Button>
             </div>
         </div>
@@ -51,6 +62,11 @@ const styles = {
         width: "5%", 
         height: "5%" 
     },
+    logoutButton: {
+        float: "right",
+        marginTop: "-50px",
+        marginRight: "10%"
+    }
 }
 
 export default Header;
