563bcbdfedbc73a12fa09191f2eb297845b9dbaf
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-08-01 02:01:16 -0500

    Calendar UI replicates design; upgraded color coding to match design

--- client/src/components/calendar/Calendar.js
+++ client/src/components/calendar/Calendar.js
@@ -1,49 +1,56 @@
 import React, { useState, useEffect } from "react";
-import moment from 'moment'
-import { CourseWeek } from "./CourseWeek"
-import { Calendar, Views, momentLocalizer }  from "react-big-calendar"
-import 'react-big-calendar/lib/css/react-big-calendar.css'
+import moment from "moment";
+import { CourseWeek } from "./CourseWeek";
+import { Calendar, Views, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./Calendar.css";
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
 
 let id = 1;
 var hexId;
 
 const dayCode2dayString = {
-    "M": "Monday",
-    "T": "Tuesday",
-    "W": "Wednesday",
-    "R": "Thursday",
-    "F": "Friday",
-    "S": "Saturday",
-    "U": "Sunday"
-}
+    M: "Monday",
+    T: "Tuesday",
+    W: "Wednesday",
+    R: "Thursday",
+    F: "Friday",
+    S: "Saturday",
+    U: "Sunday",
+};
+
+// color combos order: [background, border]
+const colorCombos = [
+    ["#F2F9FF", "#1E85E880"], // light blue
+    ["#FFFFF2", "#F5D581B3"], // light yellow
+    ["#FFFCFB", "#E35F4980"], // light orange
+    ["#FDFFFE", "#76C5AFBF"], // light green
+];
 
 const courseToCourseLabel = (course) => {
     return course.subject + " " + course.courseNum;
-}
+};
 
 const courseToTooltipLabel = (session) => {
     let tooltipString = courseToCourseLabel(session.course);
-    
+
     // add course details
-    tooltipString += '\n' + session.course.longTitle;
-    tooltipString += '\nCRN: ' + session.crn; 
-    tooltipString += '\nEnrollment: ' + session.enrollment;
-    tooltipString += '\nMax enrollment: ' + session.maxEnrollment;
-    
-    tooltipString += '\nInstructor(s): ';
+    tooltipString += "\n" + session.course.longTitle;
+    tooltipString += "\nCRN: " + session.crn;
+    tooltipString += "\nEnrollment: " + session.enrollment;
+    tooltipString += "\nMax enrollment: " + session.maxEnrollment;
+
+    tooltipString += "\nInstructor(s): ";
     // add all instructors
     for (let instructor of session.instructors) {
-        tooltipString += instructor.firstName +' ' + instructor.lastName;
+        tooltipString += instructor.firstName + " " + instructor.lastName;
     }
-    
-    return tooltipString; 
-}
+
+    return tooltipString;
+};
 
 const convertSectionToEvents = (section, session) => {
-
-
     let events = [];
     if (!section.startTime || !section.endTime) {
         return events;
@@ -53,41 +60,46 @@ const convertSectionToEvents = (section, session) => {
     let courseLabel = courseToCourseLabel(session.course);
 
     // Create moment objects for the time
-    let momentStart = moment(section.startTime, 'HH:mm');
-    let momentEnd = moment(section.endTime, 'HH:mm');
+    let momentStart = moment(section.startTime, "HH:mm");
+    let momentEnd = moment(section.endTime, "HH:mm");
 
     // create tooltip label
     let tooltipLabel = courseToTooltipLabel(session);
 
     // Create event for each day
     for (let dayCode of section.days) {
-        console.log(events)
+        console.log(events);
         // Convert the shorthand to a full weekday string
         let dayString = dayCode2dayString[dayCode];
 
         // Convert to a moment object
-        let baseDay = moment(dayString, 'dddd');
+        let baseDay = moment(dayString, "dddd");
 
-        let eventStart = baseDay.clone().add(momentStart.hour(), 'hours').add(momentStart.minute(), 'minutes');
-        let eventEnd = baseDay.clone().add(momentEnd.hour(), 'hours').add(momentEnd.minute(), 'minutes');
+        let eventStart = baseDay
+            .clone()
+            .add(momentStart.hour(), "hours")
+            .add(momentStart.minute(), "minutes");
+        let eventEnd = baseDay
+            .clone()
+            .add(momentEnd.hour(), "hours")
+            .add(momentEnd.minute(), "minutes");
 
         events.push({
             id: id++,
             title: courseLabel,
-            desc: session.course.longTitle,
+            desc: `${eventStart.format("hh:mm a")} - ${eventEnd.format(
+                "hh:mm a"
+            )}`,
             source: section,
             start: eventStart.toDate(),
             end: eventEnd.toDate(),
-            hexId: hexId
-            tooltip: tooltipLabel
+            hexId: hexId,
+            tooltip: tooltipLabel,
         });
     }
     hexId++;
     return events;
-}
-
-
-
+};
 
 /**
  * Goal is to transform each session into weekly events, in the following format:
@@ -108,81 +120,114 @@ const draftSessionsToEvents = (draftSessions) => {
         if (draftSession.visible) {
             let session = draftSession.session;
             // First convert classes
-            events = events.concat(convertSectionToEvents(session.class, session));
+            events = events.concat(
+                convertSectionToEvents(session.class, session)
+            );
             // Convert lab
-            events = events.concat(convertSectionToEvents(session.lab, session));
+            events = events.concat(
+                convertSectionToEvents(session.lab, session)
+            );
         }
     }
 
     return events;
-}
+};
 
+const slotStyleGetter = (date) => {
+    var style = {
+        font: "Medium 23px/26px Acari Sans",
+        letterSpacing: "0px",
+        color: "#8E9EB2",
+        opacity: 1,
+    };
+
+    return {
+        style: style,
+    };
+};
+
+const dayStyleGetter = (date) => {
+    var style = {
+        textAlign: "center",
+        font: "Medium 23px/26px Acari Sans",
+        letterSpacing: "0px",
+        color: "#8E9EB2",
+        opacity: 1,
+        border: "1px dashed #E4E8EE",
+        paddingTop: "16.5px",
+    };
+
+    return {
+        style: style,
+    };
+};
 
 const eventStyleGetter = (event) => {
-    console.log(event);
-    if(event.hexId % 7 == 0){
-        var backgroundColor = '#3D9970'
-    }
-    else if(event.hexId % 7 == 1){
-        var backgroundColor = '#7FDBFF'
-    }
-    else if(event.hexId % 7 == 2){
-        var backgroundColor = '#F012BE'
-    }
-    else if(event.hexId % 7 == 3){
-        var backgroundColor = '#E74C3C'
-    }
-    else if(event.hexId % 7 == 4){
-        var backgroundColor = '#FF851B'
-    }
-    else if(event.hexId % 7 == 5){
-        var backgroundColor = '#D4AC0D'
-    }
-    else if(event.hexId % 7 == 6){
-        var backgroundColor = '#C39BD3'
-    }
+    let moduloValue = event.hexId % 4;
+
+    var backgroundColor = colorCombos[moduloValue][0];
+    var borderColor = colorCombos[moduloValue][1];
+
     var style = {
+        // background:  0% 0% no-repeat padding-box;
         backgroundColor: backgroundColor,
-        borderRadius: '0px',
-        opacity: 0.8,
-        color: 'black',
-        border: '0px',
-        display: 'block'
+        border: `2px solid ${borderColor}`,
+        borderRadius: "10px",
+        opacity: 1,
+        color: "#384569",
+        display: "block",
     };
+
     return {
-        style: style
+        style: style,
     };
-}
+};
 
+const CustomClassEvent = ({ event }) => {
+    let moduloValue = event.hexId % 4;
 
+    var sidebarColor = colorCombos[moduloValue][1];
+
+    return (
+        <div className="courseEventWrapper">
+            <hr style={{ backgroundColor: `${sidebarColor}`}} className="courseEventBar" />
+            <div className="courseEvent">
+                <p id="courseCode">ARCH 225</p>
+                <p id="courseTime">09:25am - 10:30am</p>
+                <p id="courseInstructor">Reto Geiser</p>
+            </div>
+        </div>
+    );
+};
 
 const CourseCalendar = ({ draftSessions }) => {
     return (
         <div>
             <Calendar
-            events = {draftSessionsToEvents(draftSessions)}
-            step={10}
-            timeslots={3}
-            localizer={localizer}
-            defaultView={Views.WEEK}
-            formats={{ dayFormat: 'dddd' }} // Calendar columns show "Monday", "Tuesday", ...
-            views={{month: false, week: CourseWeek, day: false}}
-            drilldownView={null}
-            defaultDate={moment("Sunday", "dddd")} // Always start on Sunday of the week
-            eventPropGetter={(eventStyleGetter)}
-            toolbar={false}
-            style={style}
-            tooltipAccessor = {"tooltip"}
+                components={{ event: CustomClassEvent }}
+                events={draftSessionsToEvents(draftSessions)}
+                step={10}
+                timeslots={3}
+                localizer={localizer}
+                defaultView={Views.WEEK}
+                formats={{ dayFormat: "ddd" }} // Calendar columns show "MON", "TUES", ...
+                views={{ month: false, week: CourseWeek, day: false }}
+                drilldownView={null}
+                defaultDate={moment("Sunday", "ddd")} // Always start on Sunday of the week
+                eventPropGetter={eventStyleGetter}
+                dayPropGetter={dayStyleGetter}
+                slotPropGetter={slotStyleGetter}
+                showMultiDayTimes={true} // disable all day row
+                toolbar={false}
+                style={style}
+                tooltipAccessor={"tooltip"}
             />
         </div>
-    )
-}
+    );
+};
 
 const style = {
-    height: '100%',
-}
-
-
-
+    height: "100%",
+};
 
 export default CourseCalendar;
