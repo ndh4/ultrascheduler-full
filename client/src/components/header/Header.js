aa77cf8614caf8c63ef40cce1a498daca4c52986
Ananya Vaidya <akv4@rice.edu>
2020-08-03 23:06:03 -0700

    merged but changes not showing up in app

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -18,8 +18,8 @@ function Header() {
     let feedbackURL = "https://forms.gle/6uyRuTxKgP3n53vB6";
     let logoutURL = "https://idp.rice.edu/idp/profile/cas/logout"
 
-  // This initializes Google Analytics
-  initGA();
+    // This initializes Google Analytics
+    initGA();
 
     // Redirects people to our Medium page on a new page if they click our logo to learn more about us
     const handleLogoClick = () => {
@@ -33,37 +33,37 @@ function Header() {
 
     return (
         <div>
-            <div style={{ textAlign: "center"}}>
+            <div style={{ textAlign: "center" }}>
                 <Title />
-                <img 
-                src={RiceAppsLogo}
-                style={styles.logo}
-                onClick={() => handleLogoClick()} 
+                <img
+                    src={RiceAppsLogo}
+                    style={styles.logo}
+                    onClick={() => handleLogoClick()}
                 />
-                    <div style={styles.wrapper}>
-                        <Button
-                            variant="outlined"
-                            style={isDesktopOrLaptop ? styles.logoutButton : styles.logoutMobile}
-                            onClick={() => handleLogoutClick()}>
-                            Log Out
+                <div style={styles.wrapper}>
+                    <Button
+                        variant="outlined"
+                        style={isDesktopOrLaptop ? styles.logoutButton : styles.logoutMobile}
+                        onClick={() => handleLogoutClick()}>
+                        Log Out
                         </Button>
-                        <Button
-                            variant="outlined"
-                            onClick={() => window.open(feedbackURL, "_blank")}>
-                            Feedback?
+                    <Button
+                        variant="outlined"
+                        onClick={() => window.open(feedbackURL, "_blank")}>
+                        Feedback?
                         </Button>
-                    </div>
+                </div>
             </div>
         </div>
     );
-  };
+};
 
 const styles = {
-    logo: { 
-        float: "left", 
-        marginTop: "-70px", 
-        marginLeft: "2vw", 
-        width: "5%", 
+    logo: {
+        float: "left",
+        marginTop: "-70px",
+        marginLeft: "2vw",
+        width: "5%",
         height: "5%"
     },
     logoutButton: {
