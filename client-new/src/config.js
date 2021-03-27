0e0fee8f01e714b59ade2e5db3b7bb9de0b845f2
Will Mundy <TheWillMundy@users.noreply.github.com>
2021-03-27 13:11:51 -0500

    Renamed client-new to client

--- client-new/src/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "backendURL": process.env.REACT_APP_BACKEND_URL,
-    "gqlURL": process.env.REACT_APP_GRAPHQL_URL,
-    "serviceURL": process.env.REACT_APP_SERVICE_URL,
-    "FIREBASE_API_KEY": process.env.REACT_APP_FIREBASE_API_KEY,
-    "FIREBASE_AUTH_DOMAIN": process.env.REACT_APP_AUTH_DOMAIN
-};
--- client-new/src/constants/URLTypes.js
+++
@@ -1,6 +0,0 @@
-const URLTypes = {
-        DETAIL: 0,
-        EVAL: 1,
-};
-
-export default URLTypes
