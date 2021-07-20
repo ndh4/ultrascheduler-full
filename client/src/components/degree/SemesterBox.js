591006dff0332e99985f36c54895da89ef012aab
Chelsea Zhao <xiaoqi.zhao@rice.edu>
2021-07-20 16:06:29 +0800

    add notes js done

--- client/src/components/degree/SemesterBox.js
+++ client/src/components/degree/SemesterBox.js
@@ -3,22 +3,53 @@ import "./SemesterBox.css";
 import CourseRowBox from "./CourseRowBox";
 import TitleBox from "./TitleBox";
 import { useHistory } from "react-router";
+import Modal from 'react-modal';
+import { useState } from "react";
 
 const SemesterBox = () => {
+    // for the edit schedule button
     const history = useHistory();
+
+    // for the notes modal
+    const [modalState, setModal] = useState(false);
+    const openModal = () => {
+        setModal(true);
+    }
+    const closeModal = () => {
+        setModal(false);
+    }
+
+    // for the notes content
+    const [inputVal, changeInputVal] = useState("");
+    const saveInput = () => {
+        changeInputVal(document.getElementById("notes").value);
+        document.getElementById('notes').value = '';
+    }
 
     return (
         <div className="bigBox">
             <button
-                className="editSchedule"
+                className="button"
                 onClick={() => history.push("/schedule")}
             >
                 Edit Schedule
             </button>
-            <button className="notes" onClick="modal">
-                Notes
+
+            <button className="button" onClick={openModal}>
+                Add Notes
             </button>
-            <button className="addCustomCourse" onClick="modal">
+            <Modal isOpen={modalState} className='modal'>
+                <button onClick={closeModal} className='closeBtn'></button>
+                <div className='notesContent'>
+                <div>
+                    <input type='text' id='notes' placeholder='Write your notes here...'></input>
+                    <button onClick={saveInput}>Save</button>
+                </div>
+                    <div>{inputVal}</div>
+                </div>
+            </Modal>
+            
+            <button className="button" onClick="modal">
                 Add Custom Course
             </button>
             <div className="semesterFlexBox">
