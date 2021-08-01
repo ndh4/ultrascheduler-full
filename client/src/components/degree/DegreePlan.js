ca6403ef87ac38c52be42ca92571c3131b637ba7
Chelsea Zhao <xiaoqi.zhao@rice.edu>
2021-08-01 11:46:59 +0800

    add/delete semester done

--- client/src/components/degree/DegreePlan.js
+++ client/src/components/degree/DegreePlan.js
@@ -1,152 +1,34 @@
 import React, {useEffect} from 'react'
-import Semester from './SemesterBox'
+import SemesterBox from './SemesterBox'
 import './DegreePlan.css'
 import { gql, useMutation, useQuery } from "@apollo/client";
-
-const QUERY_ALL_USER_SCHEDULES = gql`
-    query scheduleMany {
-        scheduleMany {
-        _id
-        term
-        draftSessions {
-            session {
-            course {
-                subject
-                longTitle
-                courseNum
-                creditsMin
-                creditsMax
-                prereqs
-            }
-            instructors{
-                firstName
-                lastName
-            }
-            maxEnrollment
-            }
-            visible
-        }
-        customCourse
-        notes
-        }
-    }
-`;
-
-const FIND_CREATE_SCHEDULE = gql`
-    query scheduleOne($term: String!) {
-        scheduleOne(filter: { term: $term }) {
-        _id
-        term
-        draftSessions {
-            session {
-            course {
-                subject
-                longTitle
-                courseNum
-                creditsMin
-                creditsMax
-                prereqs
-            }
-            instructors{
-                firstName
-                lastName
-            }
-            maxEnrollment
-            }
-            visible
-        }
-        customCourse
-        notes
-        }
-    }
-`;
-
-// const MUTATION_CREATE_SCHEDULE = gql`
-//     mutation {
-//         degreePlanAddTerm(record: {**nothing in here for now**}) {
-//         term
-//         customCourse
-//         notes
-//         }
-//     }
-// `;
-
-// const MUTATION_REMOVE_SCHEDULE = gql`
-//     mutation {
-//         degreePlanRemoveTerm(record: {_id: ID!}) {
-//         _id
-//         }
-//     }
-// `;
-
-/**
- * Removes the custom course from the term
- */
-const REMOVE_CUSTOM_COURSE = gql`
-    mutation RemoveCustomCourse($scheduleID: ID!, $sessionID: ID!) {
-        scheduleRemoveSession(scheduleID: $scheduleID, sessionID: $sessionID) {
-            _id
-            term
-            draftSessions {
-                _id
-                session {
-                    _id
-                }
-                visible
-            }
-        }
-    }
-`;
+import { useState } from 'react';
 
 const DegreePlan = () => {
-    const { data, loading, error } = useQuery(QUERY_ALL_USER_SCHEDULES, {
-    });
 
-    if (loading) return <p>Loading</p>;
-    if (error) return <p>Error</p>;
-    if (!data) return <p>Error</p>;   
-    
-    console.log("Hi")
-    console.log(data.scheduleMany[1])
-    console.log("Hi")
+    // to keep the semester in a list to order them
+    const [semesterList, setSemesterList] = useState([{id: 1},]);
 
-    // let [removeCustomCourse] = useMutation(REMOVE_CUSTOM_COURSE, {
-    //     variables: { scheduleID: scheduleID, sessionID: session._id },
-    // });
+    // adding new semester to semester list (state variable)
+    const addNewSem = () => {
+        const newSem = {id: semesterList.length + 1}
+        setSemesterList([...semesterList, newSem])
+    }
 
-    // let [findOrCreateSchedule] = useQuery(FIND_CREATE_SCHEDULE, {
-    //     variables: { term: "202010"/*new String(term)*/ },
-    // });
+    const deleteSem = (id) => {
+        const updated_list = semesterList.filter((semester) => semester.id != id)
+        setSemesterList(updated_list)
+    }
 
     return (
-        <div className='layout'>
-            <Semester />
-            <Semester />
-            <Semester />
-            <Semester />
-            <Semester />
-            <Semester />
-            <Semester />
-            <Semester />
-            <Semester />
-            <Semester />
-            <Semester />
-            <Semester />
-            <Semester />
-            <p>
-            Hello, this should be the data: {JSON.stringify(data)}
-            </p>
-            {/* <button onClick={removeCustomCourse()}>
-                Remove Course
-            </button>
-
-            <button onChange={updateCustomCourse()}>
-                Update Course
-            </button> */}
-
-            {/* <button onClick={findOrCreateSchedule()}>
-                Add Term
-            </button> */}
+        <div>
+            <div className='layout'>
+            {semesterList.map(
+            (semester) => {
+            return (
+                <SemesterBox id={semester.id} deleteSem={() => deleteSem(semester.id)}/>)})}
+            <button onClick={addNewSem} className="addBtn">+</button>
+            </div>
         </div>
     )
 }
--- client/src/components/degree/SemesterBox.js
+++ client/src/components/degree/SemesterBox.js
@@ -3,10 +3,10 @@ import "./SemesterBox.css";
 import CourseRowBox from "./CourseRowBox";
 import TitleBox from "./TitleBox";
 import { useHistory } from "react-router";
-import Modal from "react-modal";
+import Modal from 'react-modal';
 import { useState } from "react";
 
-const SemesterBox = () => {
+const SemesterBox = (props) => {
     // for the edit schedule button
     const history = useHistory();
 
@@ -14,42 +14,29 @@ const SemesterBox = () => {
     const [modalState, setModal] = useState(false);
     const openModal = () => {
         setModal(true);
-    };
+    }
     const closeModal = () => {
         setModal(false);
-    };
+    }
 
     // for the notes content
     const [inputVal, changeInputVal] = useState("");
-    // const saveInput = (e) => {
-    //     changeInputVal(document.getElementById("notes").value);
-    //     document.getElementById("notes").value = inputVal;
-    // }
 
     return (
         <div className="bigBox">
+            <button onClick={props.deleteSem} style={{width:"35px"}} className="button">x</button>
             <button
-                className="button"
+                className="button" style={{width:"170px"}}
                 onClick={() => history.push("/schedule")}
             >
                 Edit Schedule
             </button>
 
-            <button className="button" onClick={openModal}>
+            <button className="button" style={{width:"170px"}} onClick={openModal}>
                 Edit Notes
             </button>
             <Modal isOpen={modalState} className='modal' onRequestClose={closeModal}>
                 <div className='notesContent'>
-                    {/* <input type='text' id='notes' placeholder='Write your notes here...' /> */}
-                    {/* <input 
-                        type='text' 
-                        id='notes' 
-                        placeholder='Write your notes here...'
-                        value={inputVal}
-                        onChange={(e) => changeInputVal(e.target.value)}
-                        style={{whiteSpace: 'pre-wrap', overflowWrap: 'break-word'}}
-                        className='textbox'
-                        /> */}
                     <textarea 
                         maxlength='400' 
                         placeholder='Write your notes here...' 
@@ -57,12 +44,10 @@ const SemesterBox = () => {
                         value={inputVal}
                         onChange={(e) => changeInputVal(e.target.value)}>
                     </textarea>
-                    {/* <button onClick={saveInput}>Save</button> */}
-                    {/* <div style={{width: "90%", wordWrap: "break-word"}}>{inputVal}</div> */}
                 </div>
             </Modal>
 
-            <button className="button" onClick="modal">
+            <button className="button" style={{width:"170px"}} onClick="modal">
                 Add Custom Course
             </button>
             <div className="semesterFlexBox">
