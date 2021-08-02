0eff461a41fa598b4fdbd6b5693e52a96a9c7c41
Quang Nguyen <nnquang.1412@gmail.com>
2021-08-02 10:22:57 +0700

    fix css of each table

--- client/src/components/degree/LeftTitleBox.js
+++ client/src/components/degree/LeftTitleBox.js
@@ -22,12 +22,11 @@ const LeftTitleBox = (props) => {
                 className="year"
                 onChange={(e) => setYear(e.target.value)}
             >
-                                <option value="2021">2021</option>
+                              <option value="2021">2021</option>
                                 <option value="2022">2022</option>
                                 <option value="2023">2023</option>
                                 <option value="2024">2024</option>
                                 <option value="2025">2025</option>
-                            
             </select>
                         
             <select
