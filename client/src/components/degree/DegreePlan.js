4247abffdf2526d3403cf400727c14eea6980428
Quang Nguyen <nnquang.1412@gmail.com>
2021-08-02 09:41:01 +0700

    .

--- client/src/components/degree/DegreePlan.js
+++ client/src/components/degree/DegreePlan.js
@@ -1,91 +1,111 @@
-import React, { useState, }from 'react'
-import SemesterBox from './SemesterBox'
-import './DegreePlan.css'
-import { gql, useQuery, useMutation} from "@apollo/client";
-
+import React, { useState, useEffect } from "react";
+import SemesterBox from "./SemesterBox";
+import "./DegreePlan.css";
+import { gql, useQuery, useMutation } from "@apollo/client";
+// query all of the schedules for a user
 const QUERY_ALL_USER_SCHEDULES = gql`
     query scheduleMany {
         scheduleMany {
-        _id
-        term
-        draftSessions {
-            session {
-            course {
-                subject
-                longTitle
-                courseNum
-                creditsMin
-                creditsMax
-                prereqs
-                coreqs
-                distribution
+            _id
+            term
+            draftSessions {
+                session {
+                    course {
+                        subject
+                        longTitle
+                        courseNum
+                        creditsMin
+                        creditsMax
+                        prereqs
+                        coreqs
+                        distribution
+                    }
+                    instructors {
+                        firstName
+                        lastName
+                    }
+                    maxEnrollment
+                }
+                visible
             }
-            instructors{
-                firstName
-                lastName
-            }
-            maxEnrollment
-            }
-            visible
-        }
-        customCourse
-        notes
+            customCourse
+            notes
         }
     }
 `;
-
+// mutation to delete semester, call from onclick the buttons
+// const MUTATION_DELETE_SEMESTER = gql`
+//     mutation
+// `;
+// mutation to add semester, call from onclick the buttons
+// const MUTATION_DELETE_SEMESTER = gql`
+//     mutation
+// `;
 const DegreePlan = () => {
-
-    const {loading, error, data} = useQuery(QUERY_ALL_USER_SCHEDULES)
-
-    if (loading) return <p>Loading</p>;
-    if (error) return <p>Error</p>;
-    if (!data) return <p>Error</p>;
-
-    // get only the data we need
-    const defaultSchedule = data.scheduleMany.map(schedule => 
-        (
-            {"term": schedule.term, 
-             "draftSessions": schedule.draftSessions, 
-             "notes": schedule.notes}
-        )
-    );
-
-    // const 
     // to keep the semester in a list to order them
-    const [semesterList, setSemesterList] = useState([{id: 1},]);
-
-    // setSemester = 
-
+    const [semesterList, setSemesterList] = useState([]);
+    // get the data from the query
+    const { loading, error, data } = useQuery(QUERY_ALL_USER_SCHEDULES);
+    // print status to page (NOTE: Raises Rending more hooks than previous... error)
+    // if (loading) return <p>Loading</p>;
+    // if (error) return <p>Error</p>;
+    // if (!data) return <p>Error</p>;
+    useEffect(() => {
+        // get only the data we need
+        // const defaultSchedule = data.scheduleMany.map(schedule =>
+        //     (
+        //         {“term”: schedule.term,
+        //         “draftSessions”: schedule.draftSessions,
+        //         “notes”: schedule.notes}
+        //     )
+        // );
+        const defaultSchedule = data?.scheduleMany.map((schedule) => ({
+            term: schedule.term,
+            draftSessions: schedule.draftSessions,
+            notes: schedule.notes,
+        }));
+        setSemesterList(defaultSchedule);
+    }, [loading, data, error]);
     // adding new semester to semester list (state variable)
     const addNewSem = () => {
-        const newSem = {id: semesterList.length + 1}
-        setSemesterList([...semesterList, newSem])
-    }
-
-    const deleteSem = (id) => {
-        const updated_list = semesterList.filter((semester) => semester.id != id)
-        setSemesterList(updated_list)
-    }
-
+        // const newSem = {}
+        // setSemesterList([...semesterList, newSem])
+    };
+    // delete a semester
+    const deleteSem = (term) => {
+        const updated_list = semesterList.filter(
+            (semester) => semester.term != term
+        );
+        setSemesterList(updated_list);
+    };
     return (
         <div>
-            <div className='layout'>
-            {/* {defaultSchedule.map((semester) => {
+            <div className="layout">
+                {/* {defaultSchedule.map((semester) => {
                 return (<SemesterBox term={semester.term} draftSessions={semester.draftSessions} notes={semester.notes} />)
             })} */}
-            {semesterList.map((semester) => {
-                return (
-                    <SemesterBox term={semester.term} 
-                                 draftSessions={semester.draftSessions} 
-                                 notes={semester.notes}
-                                 id={semester.id} 
-                                 deleteSem={() => deleteSem(semester.id)}/>
-                )
-            })}
-            <button onClick={addNewSem} className="addBtn">+</button>
+                {semesterList &&
+                    semesterList.map((semester) => {
+                        return (
+                            <SemesterBox
+                                term={semester.term}
+                                draftSessions={semester.draftSessions}
+                                notes={semester.notes}
+                                //  id={semester.id}
+                                deleteSem={() => deleteSem(semester.term)}
+                            />
+                        );
+                    })}
+                <button
+                    onClick={() => {
+                        addNewSem;
+                    }}
+                    className="addBtn"
+                >
+                    +
+                </button>
             </div>
         </div>
-    )
-}
-export default DegreePlan
+    );
+};
+export default DegreePlan;
--- client/src/components/degree/SemesterBox.js
+++ client/src/components/degree/SemesterBox.js
@@ -1,4 +1,4 @@
-import React, { useState }from "react";
+import React, { useState } from "react";
 import "./SemesterBox.css";
 import CourseRowBox from "./CourseRowBox";
 import TitleBox from "./TitleBox";
@@ -15,108 +15,113 @@ const SemesterBox = (props) => {
     const [modalState, setModal] = useState(false);
     const openModal = () => {
         setModal(true);
-    }
+    };
     const closeModal = () => {
         setModal(false);
-    }
+    };
 
     // for the notes content
     const [inputVal, changeInputVal] = useState("");
     const saveInput = (e) => {
         changeInputVal(document.getElementById("notes").value);
         document.getElementById("notes").value = inputVal;
-    }
+    };
 
     //for the add course button
-    var row = [];
-    var counter = 0;
-    const message = () => {
-        console.log("Hello World!");
-        row.push(<CourseRowBox />);
-        console.log(row);
-        counter++;
-        console.log(counter);
-    }
 
-    const defaultDraftSessions = props["draftSessions"].map(sessions => (
-        {
-        "subject": sessions.session.course.subject,
-            "courseNum": sessions.session.course.courseNum, 
-            "longTitle": sessions.session.course.longTitle, 
-            "credits": sessions.session.course.creditsMin,
-            "instructorFN": (sessions.session.instructors[0] == undefined) ? ("N/A") : (sessions.session.instructors[0].firstName),
-            "instructorLN": sessions.session.instructors[0].lastName,
-            "prereqs": sessions.session.course.prereqs,
-            "corereqs": sessions.session.course.corereqs,
-            "maxEnrollment": sessions.session.maxEnrollment
-        }
-    ));
+    const defaultDraftSessions = props["draftSessions"]
+        ? props["draftSessions"].map((sessions) => ({
+              subject: sessions.session.course.subject,
+              courseNum: sessions.session.course.courseNum,
+              longTitle: sessions.session.course.longTitle,
+              credits: sessions.session.course.creditsMin,
+              instructorFN:
+                  sessions.session.instructors[0] == undefined
+                      ? "N/A"
+                      : sessions.session.instructors[0].firstName,
+              instructorLN: sessions.session.instructors[0].lastName,
+              prereqs: sessions.session.course.prereqs,
+              corereqs: sessions.session.course.corereqs,
+              maxEnrollment: sessions.session.maxEnrollment,
+          }))
+        : [];
 
-    creditSum = defaultDraftSessions.reduce(function(sum, arr) {
-        return sum + arr.credits
-    },0);
+    creditSum = defaultDraftSessions.reduce(function (sum, arr) {
+        return sum + arr.credits;
+    }, 0);
 
-    console.log(creditSum)
-        
+    console.log(creditSum);
+
     return (
         <div className="bigBox">
-            <button onClick={props.deleteSem} style={{width:"35px"}} className="button">x</button>
             <button
-                className="button" style={{width:"170px"}}
+                onClick={props.deleteSem}
+                style={{ width: "35px" }}
+                className="button"
+            >
+                x
+            </button>
+            <button
+                className="button"
+                style={{ width: "170px" }}
                 onClick={() => history.push("/schedule")}
             >
                 Edit Schedule
             </button>
 
-            <button className="button" style={{width:"170px"}} onClick={openModal}>
+            <button
+                className="button"
+                style={{ width: "170px" }}
+                onClick={openModal}
+            >
                 Edit Notes
             </button>
-            <Modal isOpen={modalState} className='modal' onRequestClose={closeModal}>
-                <div className='notesContent'>
-                    <textarea 
-                        maxlength='400' 
-                        placeholder='Write your notes here...' 
-                        className='textbox' 
+            <Modal
+                isOpen={modalState}
+                className="modal"
+                onRequestClose={closeModal}
+            >
+                <div className="notesContent">
+                    <textarea
+                        maxlength="400"
+                        placeholder="Write your notes here..."
+                        className="textbox"
                         value={inputVal}
-                        onChange={(e) => changeInputVal(e.target.value)}>
-                    </textarea>
+                        onChange={(e) => changeInputVal(e.target.value)}
+                    ></textarea>
                 </div>
             </Modal>
 
-            <button className="button" style={{width:"170px"}} onClick="modal">
+            <button
+                className="button"
+                style={{ width: "170px" }}
+                onClick="modal"
+            >
                 Add Custom Course
             </button>
             <div className="semesterFlexBox">
-                <TitleBox term    = {props["term"]} 
-                          credits = {props["credits"]}/>
+                <TitleBox term={props["term"]} credits={props["credits"]} />
 
-                {defaultDraftSessions.map((session) => {
-                    return (<CourseRowBox subject   = {session["subject"]} 
-                                          courseNum = {session["courseNum"]} 
-                                          longTitle = {session["longTitle"]} 
-                                          credits   = {session["credits"]} 
-
-                                          instructorFN  = {session["instructorFN"]}
-                                          instructorLN  = {session["instructorLN"]}
-                                          prereqs       = {session["prereqs"]}
-                                          coreqs        = {session["coreqs"]}
-                                          maxEnrollment = {session["maxEnrollment"]}/>
-                    )
-                })}
+                {defaultDraftSessions &&
+                    defaultDraftSessions.map((session) => {
+                        return (
+                            <CourseRowBox
+                                subject={session["subject"]}
+                                courseNum={session["courseNum"]}
+                                longTitle={session["longTitle"]}
+                                credits={session["credits"]}
+                                instructorFN={session["instructorFN"]}
+                                instructorLN={session["instructorLN"]}
+                                prereqs={session["prereqs"]}
+                                coreqs={session["coreqs"]}
+                                maxEnrollment={session["maxEnrollment"]}
+                            />
+                        );
+                    })}
             </div>
         </div>
     );
 };
 
-export {creditSum};
+export { creditSum };
 export default SemesterBox;
-
-
-
-
-
-
-
-
-
-
